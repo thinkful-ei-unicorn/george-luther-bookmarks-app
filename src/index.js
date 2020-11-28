7890d7608ae852f2e76f84286f198e43e030069c
George Luther <george_l@thinkful.com>
2020-11-27 22:43:32 -0500

    basic wireframe

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+import cuid from 'cuid';
+import 'normalize.css';
+import moment from 'moment'
+import './index.css';
+
+
+// import api from './api';
+// import store from './store';
+// import shoppingList from './shopping-list';
+
+function main() {
+  console.log('DOM is loaded');
+
+}
+
+$(main);
